5091bac91af93b858d93942391a1010a64064f9b
Rudransh Bhardwaj <249.rudranshbhardwaj@gmail.com>
2023-03-27 13:58:55 +0530

    update 1

--- App.js
+++ App.js
@@ -1,3 +1,5 @@
+import React from "react";
+import ReactDOM from "react-dom";
 const heading = React.createElement("h1",{id:"heading"},"Create this is react");
 const root = ReactDOM.createRoot(document.getElementById("root"));
 console.log(heading);
