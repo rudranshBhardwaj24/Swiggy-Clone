725ec0559bf0cfbc59ac68ccc7e7d7e7f4f2af93
Rudransh Bhardwaj <249.rudranshbhardwaj@gmail.com>
2023-04-05 18:55:59 +0530

    update

--- src/components/Body.js
+++ src/components/Body.js
@@ -2,25 +2,32 @@ import RestaurantCard from "./RestaurantCard";
 import restList from "../utils/mockData";
 import { useState } from "react";
 
-
 const Body = () => {
   const arr = useState(restList);
   const resList = arr[0];
   const setResList = arr[1];
   console.log(resList);
   //console.log(setResList);
-    return(
-      <div className="body">
-        <div className="filter">
-          <button className = "filter-btn"  onClick = {() => {const resList2 = resList.filter((res) => res.data.avgRating>4); setResList(resList2)}}>Top rated restaurant</button>
-        </div>
-        <div className="res-container">
-          {
-            resList.map(restaurant => <RestaurantCard key = {restaurant.data.id}restData = {restaurant}/>)
-          }
-           </div>
+  return (
+    <div className="body">
+      <div className="filter">
+        <button
+          className="filter-btn"
+          onClick={() => {
+            const resList2 = resList.filter((res) => res.data.avgRating > 4);
+            setResList(resList2);
+          }}
+        >
+          Top rated restaurant
+        </button>
       </div>
-    )
-  }
+      <div className="res-container">
+        {resList.map((restaurant) => (
+          <RestaurantCard key={restaurant.data.id} restData={restaurant} />
+        ))}
+      </div>
+    </div>
+  );
+};
 
 export default Body;
--- src/components/RestaurantCard.js
+++ src/components/RestaurantCard.js
@@ -1,16 +1,21 @@
 import { CDN_LINK } from "../utils/constants";
 const RestaurantCard = (props) => {
-    const {restData} = props;
-    const {cloudinaryImageId, name, avgRating, cuisines, deliveryTime} = restData?.data;
-    return(
-      <div className="res-card">
-        <img className = "res-logo"alt="res-logo" src={(CDN_LINK+restData.data.cloudinaryImageId)}></img>
-        <h3 className="resName"> {name} </h3>
-        <h5 className="resCuisine"> {cuisines.join(", ")}</h5>
-        <h3 className="resRating"> {avgRating} </h3>
-        <h3 className="resDelivery"> {deliveryTime} Minutes</h3>
-      </div>
-    )
-  }
+  const { restData } = props;
+  const { cloudinaryImageId, name, avgRating, cuisines, deliveryTime } =
+    restData?.data;
+  return (
+    <div className="res-card">
+      <img
+        className="res-logo"
+        alt="res-logo"
+        src={CDN_LINK + restData.data.cloudinaryImageId}
+      ></img>
+      <h3 className="resName"> {name} </h3>
+      <h5 className="resCuisine"> {cuisines.join(", ")}</h5>
+      <h3 className="resRating"> {avgRating} </h3>
+      <h3 className="resDelivery"> {deliveryTime} Minutes</h3>
+    </div>
+  );
+};
 
 export default RestaurantCard;
