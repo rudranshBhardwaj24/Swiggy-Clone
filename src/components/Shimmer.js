d3a4f9cb25154e0a75e28e96db38a3fc032ee9bb
Rudransh Bhardwaj <249.rudranshbhardwaj@gmail.com>
2023-04-10 01:23:37 +0530

    Added Shimmer effect to the app

--- src/components/Shimmer.js
+++ src/components/Shimmer.js
@@ -1,5 +1,13 @@
 const Shimmer = () => {
-  return <h1>Shimmer Loading</h1>;
+  return (
+    <div className="restaurant-list">
+      {Array(20)
+        .fill("")
+        .map((e) => (
+          <div className="shimmer-card"></div>
+        ))}
+    </div>
+  );
 };
 
 export default Shimmer;
